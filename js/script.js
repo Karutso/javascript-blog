f5d4d3fd32f9896cf0f30230daf888b660a054fc
Krzysztof Bednarczyk <karutso65@gmail.com>
2021-02-02 13:24:51 +0100

    add selection for link and article

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 'use strict';
 const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
   
@@ -10,24 +11,22 @@ const titleClickHandler = function(event){
     for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
     }
-
     const activeArticles = document.querySelectorAll('.post');
   
     for(let activeArticle of activeArticles){
       activeArticle.classList.remove('active');
     }
   
-    /* [IN PROGRESS] add class 'active' to the clicked link */
-  
+    clickedElement.classList.add('active');
     console.log('clickedElement:', clickedElement);
   
-    /* [DONE] remove class 'active' from all articles */
-  
-    /* get 'href' attribute from the clicked link */
-  
-    /* find the correct article using the selector (value of 'href' attribute) */
-  
-    /* add class 'active' to the correct article */
+    const articleSelector = clickedElement.getAttribute('href')
+    console.log('articleselector;',articleSelector)
+
+    const targetArticle = document.querySelector(articleSelector);
+    console.log('targetArticle:', targetArticle);
+    targetArticle.classList.add('active');
+    console.log('target:', targetArticle);
   }
   
   const links = document.querySelectorAll('.titles a');
