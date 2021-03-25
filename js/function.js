581206751b7b618ca23695a0d6edaa1d33fd993a
Krzysztof Bednarczyk <karutso65@gmail.com>
2021-03-25 21:18:40 +0100

    usunięcie active z article-2

--- js/function.js
+++ js/function.js
@@ -1,24 +1,24 @@
 const optArticleSelector = '.post',
-optTitleSelector = '.post-title',
+  optTitleSelector = '.post-title',
   optTitleListSelector = '.titles';
 
 function generateTitleLinks(){
-    const titleList = document.querySelector(optTitleListSelector);
-        console.log('lista tyt', titleList);
-        titleList.innerHTML = '';
-        const articles = document.querySelectorAll(optArticleSelector);
-        console.log('artykuly', articles);
-        let html = '';
-            for(let article of articles) {
-                const articleId = article.getAttribute('id');
-                console.log('id artryk;', articleId);
-                const articleTitle = article.querySelector(optTitleSelector).innerHTML;
-                const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
-                console.log('linki', linkHTML);
-                console.log('tytul', articleTitle)
-                document.querySelector(optTitleListSelector).insertAdjacentHTML('afterbegin', linkHTML);
-                html = html + linkHTML;
-              }
-            titleList.innerHTML = html;
-            console.log('html;', html);
+  const titleList = document.querySelector(optTitleListSelector);
+  console.log('lista tyt', titleList);
+  titleList.innerHTML = '';
+  const articles = document.querySelectorAll(optArticleSelector);
+  console.log('artykuly', articles);
+  let html = '';
+  for(let article of articles) {
+    const articleId = article.getAttribute('id');
+    console.log('id artryk;', articleId);
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log('linki', linkHTML);
+    console.log('tytul', articleTitle);
+    document.querySelector(optTitleListSelector).insertAdjacentHTML('afterbegin', linkHTML);
+    html = html + linkHTML;
+  }
+  titleList.innerHTML = html;
+  console.log('html;', html);
 }
--- js/script.js
+++ js/script.js
@@ -1,37 +1,37 @@
 'use strict';
 const titleClickHandler = function(event){
-    event.preventDefault();
-    const clickedElement = this;
-    console.log('Link was clicked!');
+  event.preventDefault();
+  const clickedElement = this;
+  console.log('Link was clicked!');
   
-    /* [DONE] remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
   
-    const activeLinks = document.querySelectorAll('.titles a.active');
+  const activeLinks = document.querySelectorAll('.titles a.active');
   
-    for(let activeLink of activeLinks){
-      activeLink.classList.remove('active');
-    }
-    const activeArticles = document.querySelectorAll('.post');
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
+  const activeArticles = document.querySelectorAll('.post');
   
-    for(let activeArticle of activeArticles){
-      activeArticle.classList.remove('active');
-    }
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
   
-    clickedElement.classList.add('active');
-    console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
+  console.log('clickedElement:', clickedElement);
   
-    const articleSelector = clickedElement.getAttribute('href')
-    console.log('articleselector;',articleSelector)
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log('articleselector;',articleSelector);
 
-    const targetArticle = document.querySelector(articleSelector);
-    console.log('targetArticle:', targetArticle);
-    targetArticle.classList.add('active');
-    console.log('target:', targetArticle);
+  const targetArticle = document.querySelector(articleSelector);
+  console.log('targetArticle:', targetArticle);
+  targetArticle.classList.add('active');
+  console.log('target:', targetArticle);
    
-  }
-   generateTitleLinks();
-  const links = document.querySelectorAll('.titles a');
-  console.log('linki?', links)
-  for(let link of links){
-    link.addEventListener('click', titleClickHandler);
-  }
+};
+generateTitleLinks();
+const links = document.querySelectorAll('.titles a');
+console.log('linki?', links);
+for(let link of links){
+  link.addEventListener('click', titleClickHandler);
+}
